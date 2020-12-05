d5b177e1b1402c4139a8da48788a079b463c758f
ginxx009 <ginxx1905@gmail.com>
2020-12-05 16:32:45 +0800

    Update customer_login.php

--- admin/apk_api/customer_login.php
+++ admin/apk_api/customer_login.php
@@ -7,6 +7,7 @@ if(isset($_POST['username']) && isset($_POST['password']))
 	if($user->Customer_Login($_POST['username'],$_POST['password']))
 	{
 		echo "Login Success";
+	}
 	else
 	{
 		echo "Username or Password is Incorrect";
